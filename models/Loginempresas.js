45b16a33674d8cc32ce466e5c576504fce8e8ea5
gjagomez <36309181+gjagomez@users.noreply.github.com>
2022-12-24 18:00:44 -0600

    ultimos cambios

--- models/Loginempresas.js
+++ models/Loginempresas.js
@@ -3,35 +3,46 @@ const prisma = new PrismaClient()
 const jwt = require('jsonwebtoken')
 
 async function LoginAdmin({ email, clave }) {
-  const Usuario = await prisma.US_ADMIN.findMany({
-    where: {
-      EMAIL: {
-        equals: email, // Default value: default
+  try {
+    const Usuario = await prisma.US_ADMIN.findMany({
+      where: {
+        EMAIL: {
+          equals: email, // Default value: default
+        },
+        CLAVE: {
+          equals: clave, // Default mode
+        },
       },
-      CLAVE: {
-        equals: clave, // Default mode
+      select: {
+        ID: true,
+        EMAIL: true,
+        NOMBRE: true,
+        EMP: true,
       },
-    },
-    select: {
-      ID: true,
-      EMAIL: true,
-      NOMBRE: true,
-      EMP: true,
-    },
-  })
+    })
 
-  const token = {
-    mensaje: jwt.sign(Usuario[0], 'ja&ka', { expiresIn: '1800s' }),
+    const token = {
+      mensaje: jwt.sign(Usuario[0], 'empowerment', { expiresIn: '1800s' }),
+    }
+
+    return token
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      // console.log(e.code)
+      // if (e.code === 'P2002') {
+      //   console.log(
+      //     'There is a unique constraint violation, a new user cannot be created with this email',
+      //   )
+      // }
+    }
+    throw e
   }
-
-  return token
 }
 async function CreateUser(input) {
   const addUseradmin = await prisma.US_ADMIN.create({
     data: input,
   })
 
-  console.log(addUseradmin)
   const { empresa } = addUseradmin
   const ListUser = await prisma.US_ADMIN.findMany({
     where: {
